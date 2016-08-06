97b3a679563e015bfd5b33bc2ece2471f3d3d2e9
Uche Nnadi <ucheunnadi@gmail.com>
2016-08-06 01:25:37 -0700

    [test] v2

--- start_bots.js
+++ start_bots.js
@@ -1,7 +1,7 @@
 'use strict';
 let server = require('./server.js');
 
-require('./bots/slack_bot.js')(server).start_bot();
+// require('./bots/slack_bot.js')(server).start_bot();
 require('./bots/facebook_bot.js')(server).start_bot();
 
 // require('./twilio_bot.js')(server).start_bot();
