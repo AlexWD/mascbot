74376c937285a5adad82fcbd18f0819739dce82f
Uche Nnadi <ucheunnadi@gmail.com>
2016-08-06 01:35:50 -0700

    [fix] start_up.js v2

--- start_bots.js
+++ start_bots.js
@@ -3,7 +3,7 @@ let server = require('./server.js');
 
 let slackBot = require('./bots/slack_bot.js')(server);
 let facebookBot = require('./bots/facebook_bot.js')(server);
-slackBot..start_bot();
+slackBot.start_bot();
 facebookBot.start_bot();
 
 // require('./twilio_bot.js')(server).start_bot();
