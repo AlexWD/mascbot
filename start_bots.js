d768028b20b7736351a095f65bb6a18e57741d6a
Uche Nnadi <ucheunnadi@gmail.com>
2016-08-10 16:36:30 -0700

    deploy only slack-bot

--- start_bots.js
+++ start_bots.js
@@ -2,8 +2,9 @@
 let server = require('./server.js');
 
 let slackBot = require('./bots/slack_bot.js')(server);
-let facebookBot = require('./bots/facebook_bot.js')(server);
 slackBot.start_bot();
-facebookBot.start_bot();
+
+// let facebookBot = require('./bots/facebook_bot.js')(server);
+// facebookBot.start_bot();
 
 // require('./twilio_bot.js')(server).start_bot();
