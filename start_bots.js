00757d2f6a67e2dccb7982ea4f747ff004e98624
Uche Nnadi <ucheunnadi@gmail.com>
2016-08-14 21:44:03 -0700

    [remove] unneeded code

--- start_bots.js
+++ start_bots.js
@@ -3,8 +3,3 @@ let server = require('./server.js');
 
 let slackBot = require('./bots/slack_bot.js')(server);
 slackBot.start_bot();
-
-// let facebookBot = require('./bots/facebook_bot.js')(server);
-// facebookBot.start_bot();
-
-// require('./twilio_bot.js')(server).start_bot();
--- utils/server-configs.js
+++ utils/server-configs.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = function(server) {
-	// set view engine
-	server.set('view engine', 'ejs');
 
 	// set port
 	server.set('port', process.env.PORT || 3000);
