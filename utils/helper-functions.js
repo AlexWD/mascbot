cf9dc72da5d91b4e45ec9ce305f78c4a7bf1026d
Uche Nnadi <ucheunnadi@gmail.com>
2016-08-18 18:28:32 -0700

    [add] error handling for FB chat

--- utils/helper-functions.js
+++ utils/helper-functions.js
@@ -89,7 +89,8 @@ function sendFBBotMessage(opts) {
 	// logic still a work in progress
 	rp(routeOptions.sendFBBotOptions(opts))
 	.catch((err) => {
-		console.log('Error sending message to FB bot:', err);
+		console.log('Error sending message to FB bot:', err.body.error);
+    console.log('There was an error')
 	})
 }
 
